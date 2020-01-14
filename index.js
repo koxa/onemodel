62aa49620e39bb3980401d63b7679a37e29a60c5
koxa <konstantyn.pr@gmail.com>
2020-01-14 14:23:14 -0500

    proper export

--- index.js
+++ index.js
@@ -1,73 +1,8 @@
-require('@babel/register');
-require("core-js/stable");
-require("regenerator-runtime/runtime");
-const http = require('http');
-const express = require('express');
-const {ServerModel} = require('./onemodel/server');
-const MongoClient = require('mongodb').MongoClient;
-const app = express();
-const router = express.Router();
-
-async function run() {
-    const client = await MongoClient.connect('mongodb://127.0.0.1', {logger: console});
-    const DB = client.db('universal-model');
-
-    app.use(express.static('public'));
-    app.use(express.json());
-
-    class Book extends ServerModel {
-        static getDriver() {
-            return DB.collection(this.getCollectionName());
-        }
-
-        static getModelConfig() {
-            return Object.assign(super.getModelConfig(), {
-                lockProps: true
-            });
-        }
-
-        static getDefaultProps() {
-            return {
-                title: undefined,
-                author: undefined
-            }
-        }
-
-        static getCollectionName() {
-            return 'books';
-        }
-
-        constructor() {
-            super(...arguments);
-        }
-    }
-
-    app.route('/books/:id?')
-        .get(async (req, res) => {
-            const books = await Book.find();
-            res.json(books);
-        })
-        .post(async (req, res) => {
-            //const book = req.data();
-            const book = new Book(req.body);
-            res.json(await book.save());
-        })
-        .put(async (req, res) => {
-            const id = req.params.id;
-            const book = await Book.findById(id);
-            book.setAll(req.body);
-            res.json(await book.save());
-        });
-
-    //app.use('*', router);
-    console.log('Mongo connected');
-    http.createServer(app).listen(3000, "0.0.0.0");
-    console.log('Listening on port 3000');
-}
-
-run().catch(err => console.error(err));
-
-
-
+export {default as Model} from './onemodel/common/model/Model';
+export {default as ObservableModel} from './onemodel/common/model/ObservableModel';
+export {default as ClientModel} from './onemodel/client/model/ClientModel';
+export {default as ClientModelWrapper} from './onemodel/client/model/ClientModelWrapper';
+export {default as ServerModel} from './onemodel/server/model/ServerModel';
+export {default as ServerModelWrapper} from './onemodel/server/model/ServerModelWrapper';
 
 
