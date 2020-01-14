bdda2a9b76ba903c64f222c4156f6dfefc6d47cf
koxa <konstantyn.pr@gmail.com>
2020-01-14 14:29:09 -0500

    proper export

---
+++ onemodel/index.js
@@ -0,0 +1,8 @@
+export {default as Model} from './common/model/Model';
+export {default as ObservableModel} from './common/model/ObservableModel';
+export {default as ClientModel} from './client/model/ClientModel';
+export {default as ClientModelWrapper} from './client/model/ClientModelWrapper';
+export {default as ServerModel} from './server/model/ServerModel';
+export {default as ServerModelWrapper} from './server/model/ServerModelWrapper';
+
+
