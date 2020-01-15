aa17698db21c4ba35f63f9f7f623d31c081b83cc
koxa <konstantyn.pr@gmail.com>
2020-01-15 18:45:30 -0500

    package json webpack config updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,70 +2,72 @@ var webpack = require('webpack');
 var path = require('path');
 var nodeExternals = require('webpack-node-externals');
 
-module.exports = [{
-    mode: 'development',
-    target: 'web',
-    entry: {
-        client: './testapp/index.js'
-    },
-    output: {
-        path: __dirname + '/public',
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devtool: 'eval-cheap-module-source-map',
-}, {
-    mode: 'development',
-    // target: 'node',
-    entry: {
-        index: './src/index.js'
-    },
-    output: {
-        library: 'OneModel',
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
-        path: path.resolve('dist'),
-        filename: 'onemodel.development.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    externals: [nodeExternals()]
-}, {
-    mode: 'production',
-    // target: 'node',
-    entry: {
-        index: './src/index.js'
-    },
-    output: {
-        library: 'OneModel',
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
-        path: path.resolve('dist'),
-        filename: 'onemodel.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    externals: [nodeExternals()]
-}];
+module.exports = [
+    {
+        mode: 'development',
+        target: 'web',
+        entry: {
+            client: './testapp/index.js'
+        },
+        output: {
+            path: __dirname + '/public',
+            filename: '[name].js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js?$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        devtool: 'eval-cheap-module-source-map',
+    }, {
+        mode: 'development',
+        // target: 'node',
+        entry: {
+            index: './src/index.js'
+        },
+        output: {
+            library: 'OneModel',
+            libraryTarget: 'umd',
+            umdNamedDefine: true,
+            path: path.resolve('dist'),
+            filename: 'onemodel.development.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js?$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        externals: [nodeExternals()]
+    }, {
+        mode: 'production',
+        // target: 'node',
+        entry: {
+            index: './src/index.js'
+        },
+        output: {
+            library: 'OneModel',
+            libraryTarget: 'umd',
+            umdNamedDefine: true,
+            path: path.resolve('dist'),
+            filename: 'onemodel.js' 
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js?$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        externals: [nodeExternals()]
+    }
+];
