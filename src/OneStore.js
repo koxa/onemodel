d4c10c5aeb743f683b9598f0e0442e36356a8d78
koxa <konstantyn.pr@gmail.com>
2023-02-02 12:09:45 -0500

    minor fixes + server2

--- src/OneStore.js
+++ src/OneStore.js
@@ -5,7 +5,7 @@ import ClientStore from './client/store/ClientStore';
  * OneModel is a ClientModel when in Browser, otherwise it's ServerModel if common js modules exist
  */
 let Parent;
-if (typeof window === 'undefined' && module && module.exports) {
+if (typeof window === 'undefined' && module) {
   // it's NodeJS
   Parent = ServerStore;
 } else if (window) {
