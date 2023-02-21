21da467844552f6a4ed3c8fbb23b4ba07d24ab9c
Kostyantyn Pryyemskyy <c-pryyemskyy.kostyantyn@charter.com>
2023-02-21 12:30:46 -0500

    .

--- src/OneModel.js
+++ src/OneModel.js
@@ -5,10 +5,10 @@ import ClientModel from './client/model/ClientModel';
  * OneModel is a ClientModel when in Browser, otherwise it's ServerModel if common js modules exist
  */
 let Parent;
-if (typeof globalThis.window === 'undefined' && module) {
+if (typeof window === 'undefined' && module) {
   // it's NodeJS
   Parent = ServerModel;
-} else if (typeof globalThis.window !== 'undefined') {
+} else if (typeof window !== 'undefined') {
   Parent = ClientModel;
 } else {
   throw new Error('Unable to certainly determine environment to export OneModel');
