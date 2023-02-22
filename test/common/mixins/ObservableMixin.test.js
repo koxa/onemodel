25b7d83b8513c23cd2930024da3e5ded121aea54
Kostyantyn Pryyemskyy <c-pryyemskyy.kostyantyn@charter.com>
2023-02-22 09:52:42 -0500

    Added test for ObservableMixin.js

---
+++ test/common/mixins/ObservableMixin.test.js
@@ -0,0 +1,40 @@
+import ObservableMixin from '../../../src/common/mixins/ObservableMixin';
+
+describe('ObservableMixin', () => {
+  describe('constructor', () => {
+    test('should create instance of ObservableMixin', () => {
+      const observable = new ObservableMixin();
+      expect(observable).toBeInstanceOf(ObservableMixin);
+    });
+
+    test('should set __muted to false by default', () => {
+      const observable = new ObservableMixin();
+      expect(observable.__muted).toBe(false);
+    });
+  });
+
+  describe('mute', () => {
+    test('should set __muted to true', () => {
+      const observable = new ObservableMixin();
+      observable.mute();
+      expect(observable.__muted).toBe(true);
+    });
+  });
+
+  describe('unMute', () => {
+    test('should set __muted to false', () => {
+      const observable = new ObservableMixin();
+      observable.mute();
+      observable.unMute();
+      expect(observable.__muted).toBe(false);
+    });
+  });
+
+  describe('getEvents', () => {
+    test('should throw an error', () => {
+      expect(() => ObservableMixin.getEvents()).toThrowError(
+        'At least one event must be defined in observable class',
+      );
+    });
+  });
+});
