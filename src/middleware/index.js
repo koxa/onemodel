9765458fd68b436c0ec58990673edfbc9d480d52
koxa <konstantyn.pr@gmail.com>
2023-03-02 12:12:34 -0500

    updates to default middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,5 @@
 import http from 'http';
+import { OneModel } from "../index";
 
 /**
  * The Default OneModel HTTP Server
@@ -6,13 +7,12 @@ import http from 'http';
  * Using default Adaptor
  */
 class OneModelServer {
-  constructor(port = 3000) {
+  constructor(port = 3000, model = OneModel) {
     this.port = port;
+    this.model = model;
     this.sockets = {};
     this.nextSocketId = 0;
-    this.server = http.createServer((req, res) => {
-
-    });
+    this.server = http.createServer(this.handleReqRes);
     this.server.on('connection', function (socket) {
       let socketId = this.nextSocketId++;
       this.sockets[socketId] = socket;
@@ -28,7 +28,22 @@ class OneModelServer {
    * @param res
    */
   handleReqRes(req, res) {
-
+    switch(req.method) {
+      case 'GET':
+        // this.model.read()
+        break;
+      case 'POST':
+        // this.model.update()
+        break;
+      case 'PUT':
+        // this.model.create();
+        break;
+      case 'DELETE':
+        // this.model.delete();
+        break;
+      default:
+        throw new Error('Unknown HTTP method was used: ' + req.method);
+    }
   }
 
   async start() {
