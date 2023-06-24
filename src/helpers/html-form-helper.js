44c770672cc714ca1d299fdbcd577069e63c426c
koxa <konstantyn.pr@gmail.com>
2023-06-24 19:25:06 -0400

    improved html form helper and defined basic advanced props config

--- src/helpers/html-form-helper.js
+++ src/helpers/html-form-helper.js
@@ -1,24 +1,85 @@
-import {OneModel} from '../index';
+import { OneModel } from "../index";
 
 /**
  *
  * @param {OneModel} model
  */
 function generateForm(model) {
-    const {props} = model.getConfig();
-    let out = '<form method="post">';
-    for (let prop in props) {
-        //todo: support complex prop definitions
-        let val = props[prop];
-        out += `<p>
-            <label for="${prop}">${prop}</label>
-            <input type="text" name="${prop}" value="${val}"/>
-        </p>`;
-    }
-    out+= '<input type="submit" value="Add"/>';
-    out+= '</form>';
+  const { props } = model.getConfig();
+  let out = "<form method=\"post\">";
+  for (let prop in props) {
+    //todo: support complex prop definitions
+    out += getProp(prop, props[prop], model.get ? model.get(prop) : props[prop]); //whether instance or class
+  }
+  out += "<input type=\"submit\" />";
+  out += "</form>";
 
-    return out;
+  return out;
+}
+
+function getProp(prop, propVal, val) {
+  let html = "";
+
+  switch (typeof propVal) {
+    case "string":
+      html += `<input type="text" name="${prop}" value="${val}"/>`;
+      break;
+    case "number":
+      html += `<input type="number" name="${prop}" value="${val}"/>`;
+      break;
+    case "object":
+      if (Array.isArray(propVal)) {
+        html += getPropArray(propVal);
+      } else {
+        html += getObject(propVal);
+      }
+      break;
+    default:
+      break;
+  }
+
+  function getPropArray(options, multiple, index) {
+    let html = "";
+    html += `<select ${multiple ? 'multiple' : ''}>`;
+    for (let o of options) {
+      html += `<option value="${o}">${o}</option>`;
+    }
+    html += `</select>`;
+    return html;
+  }
+
+  function getObject(propVal) {
+    html = '';
+    let type = propVal["type"];
+    if (!type) {
+      // trying to determine type by available fields
+      if (propVal["options"]) {
+        // consider Array is options available
+        type = Array;
+      } else if (propVal["min"] || propVal["max"]) {
+        // consider Number if min or max available
+        type = Number;
+      }
+    }
+    switch (type) {
+      case String:
+        break;
+      case Number:
+        break;
+      case Array:
+        html += getPropArray(propVal['options'], propVal['multiple'], propVal['index'])
+        break;
+    }
+
+    return html;
+  }
+
+  return `
+        <p>
+          <label for="${prop}">${prop}</label>
+          ${html}
+        </p>
+    `;
 }
 
 export default generateForm;
--- testapp3/index.js
+++ testapp3/index.js
@@ -1,9 +1,34 @@
-import { OneModel, OneModelServer }  from "../src";
+import { OneModel, OneModelServer } from "../src";
+import express from 'express';
+import generateForm from "../src/helpers/html-form-helper.js";
+const port = 3000;
+const app = express();
+app.use(express.urlencoded());
 
-class User extends OneModel {}
+class Movie extends OneModel {}
 
-const index = new OneModelServer({
-    models: [User]
+Movie.configure({
+  props: {
+    title: "Movie",
+    director: "Director",
+    genre: {index: 2, options: ['action', 'drama', 'western', 'horror', 'thriller', 'comedy']},
+    decade: ['1980', '1990', '2000', '2010', '2020'],
+    qualities: {options: ['remux-2160p', 'bluray-2160p', 'web-2160p', 'hdtv-2160p'], multiple: true},
+    rating: {type: Number, min: 0, max: 10},
+    year: 1990
+  }
 });
 
-index.start();
+app.get('/', (req, res) => {
+  let html = ''
+  res.send(generateForm(new Movie()));
+});
+
+app.post('/', (req, res) => {
+  res.send(generateForm(new Movie(req.body)));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
